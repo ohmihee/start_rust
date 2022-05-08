e06e0a78292d748d8ed2b9dac22da3cd7685081a
algml <algml9603@gmail.com>
2022-05-08 16:43:34 +0900

    fix: git

--- 01/hello_cargo/target/debug/deps/hello_cargo.d
+++
@@ -1,5 +0,0 @@
-C:\Users\오미희\Desktop\ohmihee\rust_git\01\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
-
-C:\Users\오미희\Desktop\ohmihee\rust_git\01\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
-
-src\main.rs:
--- 01/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-C:\Users\오미희\Desktop\ohmihee\rust_git\01\hello_cargo\target\debug\hello_cargo.exe: C:\Users\오미희\Desktop\ohmihee\rust_git\01\hello_cargo\src\main.rs
